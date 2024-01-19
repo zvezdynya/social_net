a5f2d0cd75c6e5aedc38914a86947176a54b9ab8
zvezdynya <alya154@rambler.ru>
2024-01-19 12:12:53 +0300

    first practice whith props(likes, posts)

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={styles.item}>
             <img src="https://images6.fanpop.com/image/photos/32400000/James-james-hetfield-32495895-1232-1744.jpg"></img>
-            <div className={styles.post_text}>Hello! I'm fine and how are you?</div>
+            <div className={styles.post_text}>
+                {props.message}
+            </div>
             <div>
-                <span>like</span>
+                <span>
+                    {`${props.likes_qnt} likes`}
+                </span>
             </div>
         </div>
     )
