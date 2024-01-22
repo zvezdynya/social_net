a1dced0edd01202a60c32be3f0bb8a08cc173cba
zvezdynya <alya154@rambler.ru>
2024-01-22 21:37:13 +0300

    install react-router-dom, configured switching of components inside SP

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 const Nav = () => {
     return (
         <nav className={styles.nav}>
-            <div>
-                <a>Profile</a>
+            <div className={styles.item}>
+                <NavLink to="/profile" className={({ isActive }) => (isActive ? `${styles.active}` : "")}>Profile</NavLink>
             </div>
-            <div>
-                <a>Messages</a>
+            <div className={styles.item}>
+                <NavLink to="/dialogs" className={({ isActive }) => (isActive ? `${styles.active}` : "")}>Messages</NavLink>
             </div>
-            <div>
-                <a>News</a>
+            <div className={styles.item}>
+                <NavLink to="/news" className={({ isActive }) => (isActive ? `${styles.active}` : "")}>News</NavLink>
             </div>
-            <div>
-                <a>Music</a>
+            <div className={styles.item}>
+                <NavLink to="/music" className={({ isActive }) => (isActive ? `${styles.active}` : "")}>Music</NavLink>
             </div>
-            <div>
-                <a>Settings</a>
+            <div className={styles.item}>
+                <NavLink to="/settings" className={({ isActive }) => (isActive ? `${styles.active}` : "")}>Settings</NavLink>
             </div>
         </nav>
     )
