a95b536cd984e33350d0a9f64d834b51f5ff612a
zvezdynya <alya154@rambler.ru>
2024-01-22 21:37:13 +0300

    install react-router-dom, configured switching of components inside SP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Nav />
-      <Profile />
-    </div>
+      <div className="app-wrapper">
+        <Header />
+        <Nav />
+        <main className='profile'>
+          <Routes>
+            <Route path="/profile" 
+            element={<Profile />}/>
+          </Routes>
+          <Routes>
+            <Route path="/dialogs" 
+            element={<Dialogs />}/>
+          </Routes>
+          <Routes>
+            <Route path="/news" 
+            element={<News />}/>
+          </Routes>
+          <Routes>
+            <Route path="/music" 
+            element={<Music />}/>
+          </Routes>
+          <Routes>
+            <Route path="/settings" 
+            element={<Settings />}/>
+          </Routes>
+          
+          
+          {/* <Route path="/news" component={News}/>
+          <Route path="/music" component={Music}/>
+          <Route path="/settings" component={Settings}/> */}
+        </main>
+      </div>
   );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <main className={styles.profile}>
+        <>
             <img className={styles.profile_top_img} src="https://w.forfun.com/fetch/f5/f50017e08f611baf19a45a1f08118bcf.jpeg"></img>
             <div className={styles.profile_person_info}>
                 <img src="https://images6.fanpop.com/image/photos/32400000/James-james-hetfield-32495895-1232-1744.jpg" alt="person-avatar"></img>
@@ -17,7 +17,7 @@ const Profile = () => {
             <div>
                 Main content
             </div>
-        </main>
+        </>
     )
 }
 
