d1d4d638d7e7e23269c8fce4495365b091785a25
zvezdynya <alya154@rambler.ru>
2024-01-27 23:30:01 +0300

    stylization

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,19 @@ import styles from './Dialogs.module.scss';
 const Dialogs = () => {
     return (
         <>
-            <div className={styles.class}>Dialogs</div>
+            <div className={styles.class}>Dialogs
+                <div className={styles.dialogs_items}>
+                    <div className={`${styles.dialog} ${styles.active}`}>Dimich</div>
+                    <div className={styles.dialog}>Andrew</div>
+                    <div className={styles.dialog}>Sveta</div>
+                    <div className={styles.dialog}>Anton</div>
+                </div>
+                <div className={styles.messages}>
+                    <div className={styles.message}>Hi</div>
+                    <div className={styles.message}>How are you?</div>
+                    <div className={styles.message}>How is your it-kamasutra?</div>
+                </div>
+            </div>
         </>
     );
 }
